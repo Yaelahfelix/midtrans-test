cfe9cc23e79b7d39f405ade199888146e8922ec3
Ferdinand Felix <ffelixhermawan@gmail.com>
2025-02-16 17:26:56 +0700

    feat: post method

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -9,3 +9,16 @@ export function GET(req: NextRequest) {
     url,
   });
 }
+
+export async function POST(req: NextRequest) {
+  const url = req.url;
+  const body = req.body;
+  console.log(body);
+  console.log("hit");
+  console.log(url);
+  return NextResponse.json({
+    message: "success",
+    url,
+    body,
+  });
+}
